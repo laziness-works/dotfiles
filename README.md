fbb9f9aad73be7dab8ea2f537612ae941ec73a9a
velo <87813960+velo4@users.noreply.github.com>
2026-03-22 00:37:21 +0900

    feat: add basic installations (#1)

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # dotfiles
+
+A simple script to automate your macOS development environment setup.
+
+## 🚀 Usage
+
+Open your terminal and run the following command:
+
+```bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/laziness-works/dotfiles/refs/heads/main/install.sh)"
+```
+
+## 📦 What's Included?
+
+- Homebrew (with automatic PATH configuration)
+- Rosetta 2
+- Docker Desktop (via Homebrew Cask)
---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+install_app() {
+  local COMMAND_NAME=$1
+  local INSTALL_SCRIPT=$2
+  local POST_INSTALL_SCRIPT=$3
+  echo "┌──────────────────────────────────────────────────┐"
+  echo "│ $(printf %-49s "$COMMAND_NAME")│"
+  echo "└──────────────────────────────────────────────────┘"
+  echo "│"
+  if ! command -v "$COMMAND_NAME" &> /dev/null; then
+    echo "│ 🛠️ Installing: $INSTALL_SCRIPT"
+    eval "$INSTALL_SCRIPT"
+    local EXIT_CODE=$?
+    if [[ $EXIT_CODE -eq 0 ]];then
+      echo "│ ✅ Installation successful!"
+      if [[ -n $POST_INSTALL_SCRIPT ]];then
+        echo "│ ⚙️ Running post-install steps...: $POST_INSTALL_SCRIPT"
+        eval "$POST_INSTALL_SCRIPT"
+      fi
+    else
+      echo "│ ❌ Installation failed with exit code $EXIT_CODE"
+      exit 1
+    fi
+  else
+    echo "│ ✅ $COMMAND_NAME is already installed."
+  fi
+  echo "│"
+}
+
+brew_post_install_script() {
+  echo >> "$HOME/.zprofile"
+  echo "# Homebrew"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv zsh)"' >> "$HOME/.zprofile"
+  eval "$(/opt/homebrew/bin/brew shellenv zsh)"
+}
+install_app brew '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"' 'brew_post_install_script'
+
+install_app rosetta 'softwareupdate --install-rosetta --agree-to-license'
+
+install_app docker 'brew install --cask docker'
