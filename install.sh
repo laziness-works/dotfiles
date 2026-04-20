8221fa953665d6590822ab859ec6c08bc4de73b2
velo <87813960+velo4@users.noreply.github.com>
2026-04-20 22:42:53 +0900

    feat: support brew package installation and refactoring

--- install.sh
+++ install.sh
@@ -8,16 +8,23 @@ install_app() {
   echo "│ $(printf %-49s "$COMMAND_NAME")│"
   echo "└──────────────────────────────────────────────────┘"
   echo "│"
-  if ! command -v "$COMMAND_NAME" &> /dev/null; then
+  local ALREADY_INSTALLED=false
+  if command -v "$COMMAND_NAME" &> /dev/null; then
+    local ALREADY_INSTALLED=true
+  elif brew list "$COMMAND_NAME" &> /dev/null; then
+    local ALREADY_INSTALLED=true
+  fi
+  if [[ "$ALREADY_INSTALLED" == "false" ]]; then
     echo "│ 🛠️ Installing: $INSTALL_SCRIPT"
     eval "$INSTALL_SCRIPT"
     local EXIT_CODE=$?
-    if [[ $EXIT_CODE -eq 0 ]];then
-      source /etc/zprofile
-      if [[ -n $POST_INSTALL_SCRIPT ]];then
+    if [[ $EXIT_CODE -eq 0 ]]; then
+      if [[ -n $POST_INSTALL_SCRIPT ]]; then
         echo "│ ⚙️ Running post-install steps...: $POST_INSTALL_SCRIPT"
         eval "$POST_INSTALL_SCRIPT"
       fi
+      source /etc/zprofile
+      source "$HOME/.zprofile"
       echo "│ ✅ Installation successful!"
     else
       echo "│ ❌ Installation failed with exit code $EXIT_CODE"
@@ -35,7 +42,6 @@ brew_post_install_script() {
     echo "# Homebrew"
     echo 'eval "$(/opt/homebrew/bin/brew shellenv zsh)"'
   } >> "$HOME/.zprofile"
-  eval "$(/opt/homebrew/bin/brew shellenv zsh)"
 }
 install_app brew '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"' 'brew_post_install_script'
 
